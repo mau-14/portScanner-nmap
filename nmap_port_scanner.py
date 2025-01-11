7dd730716c537b11b0c5bbd7d3251609b2df4e84
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-11 13:02:54 +0100

    primera prueba

--- nmap_port_scanner.py
+++ nmap_port_scanner.py
@@ -20,4 +20,38 @@ print("\n****************************************************************")
 print("\n* https://github.com/mau-14                                    *")
 print("\n****************************************************************")
 
+open_ports = []
+
+while True:
+    ip_add_entered = input("\nPor favor introduce la ip que quieres escanear")
+    if ip_add_pattern.search(ip_add_entered):
+        print(f"{ip_add_entered} es una ip válida")
+        break
+
+
+while True:
+     # Se pueden escanear 65535 puertos así que no se recomienda escanearlos todos 
+    print("Por favor introduzca el rango de puertos que quieres escanear en este formato: <int>-<int> (ejemplo 80-100)")
+    port_range = input("Introduce puertos: ")
+    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    if port_range_valid:
+        port_min = int(port_range_valid.group(1))
+        port_max = int(port_range_valid.group(2))
+        break
+
+
+nm = nmap.PortScanner()
+
+for port in range(port_min, port_max + 1):
+    try:
+
+        result = nm.scan(ip_add_entered, str(port))
+        print(result)
+
+        port_status = (result['scan'][ip_add_entered]['tcp'][port]['state'])
+        print(f"Puerto {port} es {port_status}")
+
+    except:
+        
+        print(f"No se puedo escanear el puerto {port}.")
 
